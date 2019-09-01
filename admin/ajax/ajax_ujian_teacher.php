18ce42bfe641ecaf82bf949ae1934331e2237b9e
Rukanda Faridsi <rukanda.faridsi@bukalapak.com>
2019-09-01 14:42:25 +0700

    Fix bug

--- admin/ajax/ajax_ujian_teacher.php
+++ admin/ajax/ajax_ujian_teacher.php
@@ -11,6 +11,8 @@ while($r = mysqli_fetch_array($query)){
 //Membuat tombol edit soal		
 $qsoaltype = mysqli_query($mysqli, "SELECT * FROM soal_type");
 
+$btn_soal = '';
+
 while($st = mysqli_fetch_array($qsoaltype)) {
       $qsoal = mysqli_query($mysqli, "SELECT * FROM soal WHERE id_ujian='$r[id_ujian]' AND soal_type = '$st[id]'");
       $btn_soal .= '<a class="btn btn-primary btn-sm" style="margin-right: 5px; margin-bottom: 5px;" onclick="show_soal('.$r['id_ujian'].', ' . $st['id'] . ')"><i class="glyphicon glyphicon-edit"></i> ' . $st['name'] . ' &nbsp;&nbsp;<span class="label label-warning">'.mysqli_num_rows($qsoal).'</span></a>';
