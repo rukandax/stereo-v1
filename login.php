0a37c5363329016515168bbfea1b7f35cf52f1cf
nata <nacloud99@gmail.com>
2019-09-01 21:09:02 +0700

    Ganti Title

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <html>
 <head>
 
-<title>Ujian Berbasis Komputer</title>
+<title>E-Assessment</title>
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width,initial-scale=1" />
 
