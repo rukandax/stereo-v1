e3040f0b6629c6cbb80fe548144f0cc39e28a9c5
blueeyes94 <whitedragonblueeyes94@gmail.com>
2019-09-02 16:52:39 +0700

    Ganti Title Login Admin

--- admin/login.php
+++ admin/login.php
@@ -1,7 +1,7 @@
 <html>
 <head>
 
-<title>Halaman Administrator</title>
+<title>E-Assessment Administrator</title>
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width,initial-scale=1" />
 
