511d6e262723661e636ec32989a83799c2909df0
Rukanda Faridsi <rukanda.faridsi@bukalapak.com>
2019-09-14 17:51:29 +0700

    Fix login

--- admin/login.php
+++ admin/login.php
@@ -1,15 +1,11 @@
- <?php
+<?php
 session_start();
 if(!empty($_SESSION['username']) and !empty($_SESSION['password']) ){
-   header('location: index.php');
+  header('location: index.php');
 }
-
 ?>
-
 <!DOCTYPE html>
-
 <html lang="en">
-
 <head>
 
 <title>E-Assessment</title>
@@ -62,44 +58,37 @@ $(function(){
 <body>
 
 <body>
-
-
-
-        <!-- Top content -->
-        <div class="top-content">
-         
-            <div class="inner-bg">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-sm-8 col-sm-offset-2 text">
-                            <h1><strong>Welcome to</strong> E-Assessment</h1>
-                            
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-sm-6 col-sm-offset-3 form-box">
-                           <div class="form-top">
-                              <div class="form-top-left">
-                                 <h3>Login Administrator</h3>
-                                 <p>Enter your username and password to log on:</p>
-                              </div>
-                              <div class="form-top-right">
-                                 <i class="fa fa-lock"></i>
-                              </div>
-                            </div>
-                            <div class="form-bottom">
-                             
-                             <form role="form" action="" method="post" class="login-form">
-                              <div class="form-group">
-                                 <label class="sr-only" for="form-username">Username</label>
-                                    <input type="text" name="username" placeholder="NIP" class="form-username form-control" id="form-username">
-                                 </div>
-                                 <div class="form-group">
-                                    <label class="sr-only" for="form-password">Password</label>
-                                    <input type="password" name="password" placeholder="Password" class="form-password form-control" id="form-password">
-                                 </div>
-                                 <button class="btn">Sign in!</button>
-                             </form>
+  <!-- Top content -->
+  <div class="top-content">
+    
+    <div class="inner-bg">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-8 col-sm-offset-2 text">
+            <h1><strong>Welcome to</strong> E-Assessment</h1>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-6 col-sm-offset-3 form-box">
+            <div class="alert alert-danger" role="alert"></div>
+            <div class="form-top">
+              <div class="form-top-left">
+                <h3>Login Administrator</h3>
+                <p>Enter your username and password to log on:</p>
+              </div>
+              <div class="form-top-right">
+                <i class="fa fa-lock"></i>
+              </div>
+            </div>
+            <div class="form-bottom">
+              <form role="form" action="" method="post" class="login-form">
+              <div class="form-group">
+                <label class="sr-only" for="form-username">Username</label>
+                  <input type="text" name="username" placeholder="Username" class="form-username form-control" id="form-username">
+                </div>
+                <div class="form-group">
+                  <label class="sr-only" for="form-password">Password</label>
+                  <input type="password" name="password" placeholder="Password" class="form-password form-control" id="form-password">
                 </div>
                 <button class="btn">Sign in!</button>
               </form>
