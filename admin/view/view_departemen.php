a0f29714d0352c289b983ae3c22be5034cb10a04
blueeyes94 <whitedragonblueeyes94@gmail.com>
2019-09-15 14:54:02 +0700

    Rapihkan coding di view departemen

--- admin/view/view_departemen.php
+++ admin/view/view_departemen.php
@@ -1,9 +1,11 @@
 <script type="text/javascript" src="script/script_departemen.js"> </script>
 
 <?php
+
 session_start();
 if(empty($_SESSION['username']) or empty($_SESSION['password']) or $_SESSION['leveluser']!="admin"){
    header('location: ../login.php');
+
 }
 
 include "../../library/function_view.php";
@@ -17,4 +19,5 @@ create_table(array("Nama Departemen", "Aksi"));
 open_form("modal_departemen", "return save_data()");
    create_textbox("Nama Departemen", "nama_departemen", "text", 4, "", "required");
 close_form();
+
 ?>
